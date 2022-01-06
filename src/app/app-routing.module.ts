c94313b900da0db79e7e767f4c189c4aae2dac6b
ismailkrc57 <ismailkrc57@gmail.com>
2022-01-07 01:00:28 +0300

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ThesisListComponent} from "./components/thesis-list/thesis-list.component";
 import {AuthorComponent} from "./components/author/author.component";
 import {ThesisAddComponent} from "./components/thesis-add/thesis-add.component";
 import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
-
+  {
+    path: '',
+    redirectTo: "theses",
+    pathMatch: "full",
+    component: HomeComponent
+  },
   {
     path: "theses",
     component: HomeComponent,
@@ -19,11 +23,7 @@ const routes: Routes = [
     path: "thesis-add",
     component: ThesisAddComponent
   },
-  {
-    path: '',
-    redirectTo: "theses",
-    pathMatch: "full"
-  }
+
 ];
 
 @NgModule({
