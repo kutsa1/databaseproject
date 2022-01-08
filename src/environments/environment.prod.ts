f385846d11e233c11ce239442f3c0be4139a8406
ismailkrc57 <ismailkrc57@gmail.com>
2022-01-08 12:48:32 +0300

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:8070/api/',
-  apiUrl: 'http://databaseproject.azurewebsites.net/api/'
+  apiUrl: 'https://databaseproject.azurewebsites.net/api/'
 };
