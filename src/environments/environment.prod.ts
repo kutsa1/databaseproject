eabb1698650fe5855d49865b02bd737fc48b2497
ismailkrc57 <ismailkrc57@gmail.com>
2022-01-08 12:44:59 +0300

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8070/api/'
+  // apiUrl: 'http://localhost:8070/api/',
+  apiUrl: 'http://databaseproject.azurewebsites.net/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8070/api/'
+  // apiUrl: 'http://localhost:8070/api/',
+  apiUrl: 'http://databaseproject.azurewebsites.net/api/'
 };
 
 /*
